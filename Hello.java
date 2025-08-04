0090aa0d504c0f583790d6a4941ad3c9e6d36621
sainat9392 <ppvsainath@gmail.com>
2025-08-04 11:06:10 +0530

    commiting code

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int sum = add(5, 10);
+        System.out.println("Sum: " + sum);
+    }
+}
